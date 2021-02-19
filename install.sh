f94da0e4a95f8578a24815e763bf2c193ea68e71
0x1byte <79261587+0x1byte@users.noreply.github.com>
2021-02-19 18:31:17 +0330

    Update install.sh
    add some functions to make installation readable and easy

--- install.sh
+++ install.sh
@@ -16,31 +16,45 @@ else
 	echo "your os is not supporteed yet!"
 	exit
 fi
-
+#check python version and installation
 haspython=$(python3.7 -V 2>&1 | grep -Po '(?<=Python )(.+)')
-
-if [ "$os" == "centos" ];then
-read -p "do you want to install mtproto proxy? [y/n]" ANS
-if [ "$ANS" == "y" ];then
- yum install epel-release -y && yum install tmux bind-utils screen zip git htop wget -y
-if [[ -z "$haspython" ]]
-then
+#install python 
+installpython() {
  yum install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel -y && cd /usr/src && wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz && tar xzf Python-3.7.3.tgz && cd Python-3.7.3 && ./configure --enable-loadable-sqlite-extensions && make && make install
-fi
- echo "installing mtproto proxy server..."
- git clone -b stable https://github.com/alexbers/mtprotoproxy
- echo "done! now run this command:"
- echo "cd mtprotoproxy && tmux new -s proxy and then python3.7 mtprotoproxy.py"
-elif [ "$ANS" == "n" ];then
-read -p "do you want to install bot requierments? [y/n]" ANS
-if [ "$ANS" == "y" ];then
- read -p "do you need redis? [y/n]" ANS
+}
+#mtproto proxy installation
+installmtprotoproxy() {
+ yum install epel-release -y && yum install tmux bind-utils screen zip git htop wget -y
+ if [[ -z "$haspython" ]]
+ then
+  installpython
+ fi
+  echo "installing mtproto proxy server..."
+  git clone -b stable https://github.com/alexbers/mtprotoproxy
+  echo "done! now run this command:"
+  echo "cd mtprotoproxy && tmux new -s proxy and then python3.7 mtprotoproxy.py"
+}
+#bot requierment installation
+installbot() {
+read -p "do you need redis? [y/n]" ANS
  if [ "$ANS" == "y" ];then
   yum install epel-release -y && yum install redis tmux bind-utils screen zip git htop wget -y && systemctl enable redis && systemctl start redis
  else
   yum install epel-release -y && yum install tmux bind-utils screen zip git htop wget -y
  fi
-  yum install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel -y && cd /usr/src && wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz && tar xzf Python-3.7.3.tgz && cd Python-3.7.3 && ./configure --enable-loadable-sqlite-extensions && make && make install
+}
+#main
+if [ "$os" == "centos" ];then
+read -p "do you want to install mtproto proxy? [y/n]" ANS
+if [ "$ANS" == "y" ];then
+ installmtprotoproxy
+elif [ "$ANS" == "n" ];then
+read -p "do you want to install bot requierments? [y/n]" ANS
+if [ "$ANS" == "y" ];then
+ installbot
+  if [[ -z "$haspython" ]];then
+  installpython
+ fi
 fi
 fi
 else
